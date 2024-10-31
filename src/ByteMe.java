7f870655257019b80506dcddda075cd92e74dc08
Anirudh-23090 <anirudh23090@iiitd.ac.in>
2024-10-31 11:30:30 +0530

    added new classes(Menu, Item, Customer, Order, Cart)

--- src/ByteMe.java
+++ src/ByteMe.java
@@ -1,2 +1,5 @@
 public class ByteMe {
+    public void run() {
+        System.out.println("Welcome to ByteMe");
+    }
 }
---
+++ src/Order.java
@@ -0,0 +1,39 @@
+public class Order {
+    private int date, orderID;
+    private long total;
+    private Cart cart;
+    private Customer customer;
+    private String name, status, request, details;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Cart getCart() {
+        return cart;
+    }
+    public Customer getCustomer() {
+        return customer;
+    }
+    public int getOrderID() {
+        return orderID;
+    }
+    public int getDate() {
+        return date;
+    }
+    public long getTotal() {
+        return total;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public String getRequest() {
+        return request;
+    }
+    public String getStatus() {
+        return status;
+    }
+}
